340d3d7d8e8a85499a58a1ce77fd8b7fea6a7670
Link Eduardo <link.eduardo1287@gmail.com>
2025-07-03 22:39:50 -0300

    base criada

---
+++ src/controllers/pageController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+
+export const home = (req: Request, res: Response) => {
+
+}
+
+export const comercios = (req: Request, res: Response) => {
+
+}
+
+export const lojas = (req: Request, res: Response) => {
+
+}
+
+export const servicos = (req: Request, res: Response) => {
+
+}
---
+++ src/routes/index.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import * as PageController from '../controllers/pageController';
+import * as SearchController from '../controllers/searchController';
+
+const router = Router();
+
+router.get('/', PageController.home);
+router.get('/comercios', PageController.comercios);
+router.get('/lojas', PageController.lojas);
+router.get('/servicos', PageController.servicos);
+
+router.get('/search', SearchController.search);
+
+export default router;
